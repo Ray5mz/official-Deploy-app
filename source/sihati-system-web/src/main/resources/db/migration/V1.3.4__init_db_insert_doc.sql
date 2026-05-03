2f2984c73e5fa3affa1d97d2976a8761dd079d67
Ray5mz <136928230+Ray5mz@users.noreply.github.com>
2026-05-04 00:16:36 +0100

    fixing

--- source/sihati-system-web/src/main/resources/db/migration/V1.3.4__init_db_insert_doc.sql
+++ source/sihati-system-web/src/main/resources/db/migration/V1.3.4__init_db_insert_doc.sql
@@ -4,9 +4,3 @@ ALTER TABLE care.medical_report
 
 ALTER TABLE care.medical_report
     ADD COLUMN file_name character varying(255);
-
-
-update care.medical_report set document_pdf =pg_read_binary_file('C:/Ordonnance_3.pdf'), file_name='Ordonnance_3.pdf';
-
-update care.medical_report set document_pdf =pg_read_binary_file('C:/Ordonnance_2.pdf'), file_name='Ordonnance_2.pdf';
-update care.medical_report set document_pdf =pg_read_binary_file('C:/Ordonnance_1.pdf'), file_name='Ordonnance_1.pdf';
